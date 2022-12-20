7355ca8622227e82b3afbab0a7b3ff35f73f07e2
Devoloper <as>
2022-12-20 10:05:19 +0500

    added some models and updates

--- blog/views.py
+++ blog/views.py
@@ -10,14 +10,26 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, id):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
+# def post_detail(request, id):
+#     # try:
+#     #     post = Post.published.get(id=id)
+#     # except Post.DoesNotExist:
+#     #     raise Http404("No Post found.")
+#     post = get_object_or_404(Post,
+#                              id=id,
+#                              status=Post.Status.PUBLISHED)
+#     return render(request,
+#                   'blog/post/detail.html',
+#                   {'post': post})
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
