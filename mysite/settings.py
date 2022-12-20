9e3850a0d47d1a7fe4394ab5b0788ecd03a54722
Devoloper <as>
2022-12-20 16:36:19 +0500

    added new view

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+"""To send messages to other emails and 
+    Email server configuration"""
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'abbosr180@@gmail.com'
+EMAIL_HOST_PASSWORD = 'qfagewlzgiwhmklw'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
