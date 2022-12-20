f1b7d228b0240e003a22df791a696071bbdc7119
Devoloper <as>
2022-12-20 11:25:01 +0500

    mended bags

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,18 @@ def post_list(request):
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
 
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
 # def post_detail(request, id):
 #     # try:
 #     #     post = Post.published.get(id=id)
