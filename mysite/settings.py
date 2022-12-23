f40c7312b06f5bea5beaba3ced3082346a4f8870
Devoloper <as>
2022-12-23 18:22:59 +0500

    added feed part

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,6 +26,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
 
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
